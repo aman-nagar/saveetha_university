647021d53584f9604e447eefa1387462099073dc
Aman Nagar <78151326+aman-nagar@users.noreply.github.com>
2026-02-18 19:35:00 +0530

    Remove BrowserRouter from main.jsx
    Removed BrowserRouter wrapping from App component.

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,9 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename="/saveetha_university">
-      <App />
-    </BrowserRouter>
+    <App />
   </StrictMode>,
 );
